c7a0019bcc0343a43943a78202d1f0cea87701b0
Daniel Jones <daniel.jones2000@gmail.com>
2019-09-12 13:12:25 -0700

    fixed remove bug

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -23,16 +23,18 @@ export const carReducer = (state = intialState, action) => {
     case ADD_FEATURE:
       return {
         ...state,
-        additionalPrice: state.additionalPrice + action.payload.price,
+        additionalPrice: (state.additionalPrice += action.payload.price),
         car: { ...state.car, features: [...state.car.features, action.payload] }
       };
     case REMOVE_FEATURES:
       return {
         ...state,
-        additionalPrice: state.additionalPrice - action.payload.price,
+        additionalPrice: (state.additionalPrice -= action.payload.price),
         car: {
           ...state.car,
-          features: [...state.car.features, action.payload]
+          features: state.car.features.filter(
+            feature => feature.id !== action.payload.id
+          )
         }
       };
     default:
